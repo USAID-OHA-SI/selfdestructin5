941c7b17c93832aae39d3934993dd0e4ab5e00aa
mmm2365 <74432178+mmm2365@users.noreply.github.com>
2021-05-12 14:03:06 -0700

    cleaning, changing percent
    forgot to commit

--- Scripts/testmaddie.R
+++ Scripts/testmaddie.R
@@ -35,66 +35,83 @@ load_secrets()
 
 cntry_ou <-  si_path() %>% 
   return_latest("OU_IM") %>% 
-  read_rds()   
-
+  read_msd()   
+  #read_rds()
 
 
 # MER Data Munge ---------------------------------------------------------------------------------
 
 
 briefer <- cntry_ou %>%
-  #rename(countryname = countryname) #Not sure if this is what I need to do?
   filter(fiscal_year %in% c(2021, 2020, 2019),
+         countryname == "Tanzania",
          indicator %in% c("HTS_TST", "HTS_TST_POS", "PrEP_NEW", "TX_CURR", "TX_NEW", "VMMC_CIRC"),
          standardizeddisaggregate %in% c("Total Numerator"),
-        #"Age/Sex/HIVStatus", "Modality/Age/Sex/Result", "KeyPop/Result", 
-        #"Age/Sex/ARVDispense/HIVStatus", "PregnantOrBreastfeeding/HIVStatus",
-        #"KeyPop/HIVStatus", "Age Aggregated/Sex/HIVStatus", "Modality/Age Aggregated/Sex/Result", "Age/Sex",
-         #"KeyPopAbr", "TechFollowUp>14days/Sex", "Technique/Sex", "TechFollowUp/Sex"),
          !fundingagency %in% c("Dedup")) %>%
   glamr::clean_agency() %>%
-  group_by(fiscal_year, countrynamename, indicator, fundingagency) %>%
+  group_by(fiscal_year, countryname, indicator, fundingagency) %>%
   summarise_at(vars(targets:cumulative),sum,na.rm=TRUE) %>%
   ungroup() %>%
   select(-c(qtr1:qtr4)) %>%
   mutate(
-    achievement = (cumulative/targets))
-#need to group anything not CDC and USAID into Other
+    achievement = (cumulative/targets), 
+    across(c(cumulative, targets), comma),
+    across(c(achievement), percent, .1))
+
+  #?across
+  #across(c(achievement), ~ percent(.)/100))
   
   
-  #disagg<-briefer %>%
-  #   filter(indicator %in% c("HTS_TST", "HTS_TST_POS", "PrEP_NEW", "TX_CURR", "TX_NEW", "VMMC_CIRC")) %>%
-  #   distinct(standardizeddisaggregate)
-  #   print(disagg)
-    
-    #GT!!!!!!!!
 
 #clean clean up FY
 briefer <- briefer %>%
     mutate(fiscal_year = glue("FY{str_sub(fiscal_year, start = 3)}"))
 
 #reshape
-  briefer <- briefer %>% 
+  briefer <- briefer %>%
+    arrange(indicator) %>%
+    #reorder()
     rename(results = cumulative) %>% 
     rename_with(.cols = c(targets, results, achievement), str_to_sentence) %>% 
     pivot_wider(names_from = fiscal_year,
                 # names_sep = " ",
                 names_glue = "{fiscal_year}<br>{.value}",
+                #names_sort = TRUE,
                 values_from = c(Targets, Results, Achievement)
-                ) 
+                )
+
+
+  
+
+
+ briefer <- briefer[, c("countryname", "indicator", "fundingagency", "FY19<br>Results",
+                        "FY19<br>Targets", "FY19<br>Achievement",
+                        "FY20<br>Results", "FY20<br>Targets", "FY20<br>Achievement",
+                        "FY20<br>Results", "FY20<br>Targets", "FY20<br>Achievement")]
+    
+#briefer <- briefer %>% pivot_longer(cols = fundingagency,
+                                    #values_to = "indicator")
+ 
 
 ## TO DO
   #reorder indicators (order string variables in R)
-  #reorder columns
-  #reorder agency
+  #reorder columns #reorder columns:
+  #FIGURE OUT AN EASIER WAY TO DO THIS
+  #FY19 Targets FY19 Results FY19 Achievement
+  #FY20 Targets FY20 Results FY20 Achievement
+  #FY21 Targets FY21 Results FY21 Achievement
+  
+  #reorder agency - STILL NEED TO DO THIS 
   
 ## Table
-  #select 1 counry
+  #select 1 country
+#first i need to figure out how to choose one , then I think i can use a tibble to transform
   cntry_sel <- "Tanzania"
   
   
   table_data <- briefer %>% 
     filter(cntry_sel)
+  
   
 #TO DO Table
   #add commas & percents
