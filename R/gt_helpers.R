8141e4227a1e5b421525906b0f4170ad3229a1d0
tessam30 <tim.essam@gmail.com>
2021-08-09 17:14:12 -0400

    extra space in documentation removed

--- R/gt_helpers.R
+++ R/gt_helpers.R
@@ -1,60 +1,64 @@
 # GT functions for table creation
-#'  Create change note for gt theme
-#'  @keywords internal  
-#'  @family gt helpers
+#' Create change note for gt theme
+#' @keywords internal  
+#' @family gt helpers
 change_footnote <- function() {"Number reflects percentage change from the same quarter in the previous year."}
 
 
-#'  Create dedup note for gt theme
-#'  @keywords internal
-#'  @family gt helpers
+#' Create dedup note for gt theme
+#' @keywords internal
+#' @family gt helpers
 dedup_footnote <- function() {"ALL OTHER AGENCIES based on aggregates excluding de-duplication."}
 
-#'  Create caveat note for gt theme
-#'  @keywords internal
-#'  @family gt helpers
+#' Create caveat note for gt theme
+#' @keywords internal
+#' @family gt helpers
 caveats_footnote <- function() {"Certain mechanisms have been omitted. See the Known Issues Tracker for full list of mechanisms omitted."}
 
-#'  Create delta note for gt theme
-#'  @keywords internal
-#'  @family gt helpers
+#' Create delta note for gt theme
+#'  
+#' @keywords internal
+#' @family gt helpers
 delta_footnote <- function() {"Number reflects the change between current and most recent quarter."}
 
-#'  Create vlc change note for gt theme
-#'  @keywords internal
-#'  @family gt helpers
+#' Create vlc change note for gt theme
+#'  
+#' @keywords internal
+#' @family gt helpers
 vlc_footnote <- function() {"Viral Load Covererage = TX_PVLS_D / TX_CURR_2_period_lag"}
 
-#'  Create past_fy object for gt theme
-#'  @keywords internal
-#'  @param pd of the format FYXXQX that is returned from [gophr::identifypd()]
-#'  @return a string
-#'  @family gt helpers
-#'  
+#' Create past_fy object for gt theme
+#' @keywords internal
+#' @param pd of the format FYXXQX that is returned from [gophr::identifypd()]
+#' @return a string
+#' @family gt helpers
+#' 
 past_fy <- function(pd) {paste0("FY", pd %>% substr(., 3, 4) %>% as.numeric() - 1, " Results") %>% glue::as_glue()}
 
-#'  Create present fy object for gt theme
-#'  @keywords internal
-#'  @param pd of the format FYXXQX that is returned from [gophr::identifypd()]
-#'  @return a string
-#'  @family gt helpers
+#' Create present fy object for gt theme
+#' 
+#' @keywords internal
+#' @param pd of the format FYXXQX that is returned from [gophr::identifypd()]
+#' @return a string
+#' @family gt helpers
 #'  
 present_fy <- function(pd) {paste(pd %>% substr(., 1, 4), "Cumulative") %>% glue::as_glue()}
 
-#'  Create present quarter object for gt theme
-#'  @keywords internal
-#'  @param pd of the format FYXXQX that is returned from [gophr::identifypd()]
-#'  @return a string 
-#'  @family gt helpers
+#' Create present quarter object for gt theme
+#' 
+#' @keywords  internal 
+#' @param pd of the format FYXXQX that is returned from [gophr::identifypd()]
+#' @return a string 
+#' @family gt helpers
 #'  
 present_qtr <- function(pd) { paste(pd, "Results") %>% glue::as_glue()}
 
-#'  Create author footnote for gt theme
-#'  @keywords internal
-#'  @param msd_source source metadata recovered from [selfdestructin5::msd_period()]
-#'  @return  a string
-#'  @family gt helpers
-#'  
+#' Create author footnote for gt theme
+#' @keywords internal
+#' @param msd_source source metadata recovered from [selfdestructin5::msd_period()]
+#' @return  a string
+#' @family gt helpers
+#' 
 authors_footnote <- function(msd_source){glue::glue("Created by Core Analytics Cluster on {Sys.Date()} using {msd_source}")}
 
 #' Return msd metadata for pd object
